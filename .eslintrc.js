189f4b66c12a587b75df020f935dc0361b341e37
Lars Bergqvist <lars.g.bergqvist@gmail.com>
2022-01-03 18:34:16 +0100

    Package upgrade (#28)
    * eslint fixes
    
    * Update cd.yml

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,8 @@ module.exports = {
     },
     'extends': [
         'eslint:recommended',
-        'plugin:react/recommended'
+        'plugin:react/recommended',
+        'react-app'
     ],
     'parserOptions': {
         'ecmaFeatures': {
